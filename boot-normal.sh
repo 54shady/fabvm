4d9a35bcbeaca60867401d72349e98eb9d3d63ec
zeroway <M_O_Bz@163.com>
2020-02-27 20:35:11 +0000

    Normal boot script, usb tablet, qxl graphic card

---
+++ boot-normal.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# create disk image
+# qemu-img create -f qcow2 disk.img 15G
+
+# install from iso
+# ./boot-normal.sh -hda disk.img -cdrom /path/to/iso/os.iso
+
+# normal boot
+# ./boot-normal.sh -hda disk.img -name "vm name"
+#
+# Using spice protocol
+# remote-viewer spice://localhost:3001
+
+exec qemu-system-x86_64 \
+	-enable-kvm \
+	-cpu host,kvm=off \
+	-machine pc-i440fx-2.7,accel=kvm,usb=off,dump-guest-core=off,mem-merge=off \
+	-usb \
+	-device usb-tablet \
+	-smp 4 \
+	-m 4096 \
+	-boot d \
+	$@ \
+	-vnc 0.0.0.0:1 \
+	-rtc base=localtime,clock=vm,driftfix=slew \
+	-no-hpet \
+	-no-shutdown \
+	-global PIIX4_PM.disable_s3=1 \
+	-global PIIX4_PM.disable_s4=0 \
+	-object iothread,id=iothread1 \
+	-object iothread,id=iothread2 \
+	-no-user-config \
+	-vga qxl \
+	-nodefaults
