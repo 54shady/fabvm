a29662cfcaf31e2acb80d46e4a819c5d6052ead8
zeroway <M_O_Bz@163.com>
2020-05-19 11:37:09 +0800

    RemoteTest: fix ipaddr, thread safe print, novalid connect error handle

--- run_batch_vm_test.py
+++ run_batch_vm_test.py
@@ -7,18 +7,24 @@ import random
 import paramiko
 import argparse
 
-def start_routine_d(arg):
-    ipaddr = "172.28.107.11%d" % arg
+def remote_test_routine(arg, lock):
+    ipaddr = "172.28.107.%d" % (110 + arg)
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    s.connect(hostname=ipaddr, username="root",password="0")
-    order = '/root/lmbench3/bin/arm-linux/stream'
-    stdin,stdout,stderr=s.exec_command(order)
-    result = stderr.read()
-    print("===Testing %s Start===" % ipaddr)
-    for i in range(0, len(result.splitlines())):
-        print(result.splitlines()[i])
-    print("===Testing %s End===\n" % ipaddr)
+    try:
+        s.connect(hostname=ipaddr, username="root",password="0")
+    except paramiko.ssh_exception.NoValidConnectionsError as err:
+        print("NoValidConnectionsError : " + ipaddr)
+    else:
+        order = '/root/lmbench3/bin/arm-linux/stream'
+        stdin,stdout,stderr=s.exec_command(order)
+        result = stderr.read()
+        lock.acquire()
+        print("===Testing %s Start===" % ipaddr)
+        for i in range(0, len(result.splitlines())):
+            print(result.splitlines()[i])
+        print("===Testing %s End===\n" % ipaddr)
+        lock.release()
 
 
 if __name__ == '__main__':
@@ -26,7 +32,9 @@ if __name__ == '__main__':
     parser.add_argument("-t", "--thread", type=int,
                         help="numbers of threads")
 
+    mutex = threading.Lock()
+
     args = parser.parse_args()
     for i in range(args.thread):
-        t = threading.Thread(target=start_routine_d, args=(i+1,))
+        t = threading.Thread(target=remote_test_routine, args=(i+1, mutex,))
         t.start()
