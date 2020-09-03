d6254770a496bc6a935dba17ed73fd0fee3c3d1c
zeroway <M_O_Bz@163.com>
2020-09-03 14:16:08 +0800

    NAT: clean the nat setup when vm down

--- boot-nat.sh
+++ boot-nat.sh
@@ -1,14 +1,12 @@
-# usage ./nat_vm.sh vm.qcow2
+# usage ./boot-nat.sh vm.qcow2
 
 # config the iptables for DNAT
 # ./dnat.sh
 
-# clean all the nat config
-# ./cleannat.sh
-
 sudo qemu-system-x86_64 \
 	-enable-kvm -cpu host \
 	-m 2048 -smp 2 \
-	-netdev tap,id=nd0,ifname=tap0,script=./bridge_nat_helper \
+	-netdev tap,id=nd0,ifname=tap0,script=./nat_up,downscript=./nat_down \
 	-device e1000,netdev=nd0,mac=52:54:00:12:34:27 \
+	-vnc 0.0.0.0:2 \
 	$@
