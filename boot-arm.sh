eb904f905f331e8b50278caf73e13d48a84e27b1
zeroway <M_O_Bz@163.com>
2021-03-02 10:03:38 +0800

    EFI entry name and default network mode change to nat
    Some distro default efi bootloader name
    should manaually config in nvram
    
    The fallback efi loader name is "/boot/EFI/BOOT/BOOTX64.EFI"
    so, if the new install system default efi name is something else
    it should set property to the new name in nvram, so we need add a
    nvram pflash to qemu.

--- boot-arm.sh
+++ boot-arm.sh
@@ -42,12 +42,24 @@
 # for example : set cpu run on range cpu from 100 to 120
 #exec taskset -c 100-120 qemu-system-aarch64 \
 
+gen_efi_firm()
+{
+	dd if=/dev/zero bs=1M count=64 of=QEMU_64M_CODE.fd
+	dd if=/dev/zero bs=1M count=64 of=QEMU_64M_VARS.fd
+	dd if=/usr/share/AAVMF/AAVMF_CODE.fd bs=1M of=QEMU_64M_CODE.fd conv=notrunc
+}
+
+# /dev/vda1 is the EFI partition
+# efibootmgr -c -w -L "myubuntu" -d /dev/vda -p 1 -l EFI/ubuntu/grubaa64.efi
+[ ! -f QEMU_64M_VARS.fd -o ! -f QEMU_64M_CODE.fd  ] && gen_efi_firm
+
 exec qemu-system-aarch64 \
 	-enable-kvm \
 	-cpu host \
 	-machine virt,accel=kvm,usb=off,dump-guest-core=off,gic-version=3 \
 	-name guest=demo,debug-threads=on \
-	-drive file=/usr/share/AAVMF/AAVMF_CODE.fd,if=pflash,format=raw,unit=0,readonly=on \
+	-drive file=./QEMU_64M_CODE.fd,if=pflash,format=raw,unit=0,readonly=on \
+	-drive file=./QEMU_64M_VARS.fd,if=pflash,format=raw,unit=1 \
 	-m 4G \
 	-smp 8 \
 	-no-user-config \
@@ -61,11 +73,12 @@ exec qemu-system-aarch64 \
 	-device qemu-xhci,id=usbhub1 \
 	-device usb-kbd,bus=usbhub1.0 \
 	-device usb-tablet,bus=usbhub1.0 \
-	-netdev tap,id=hostnet0,vhost=on,script=./bridge_up.sh,downscript=./bridge_down.sh \
-	-device virtio-net-pci,netdev=hostnet0,id=net0,mac=52:54:00:1b:fa:b2 \
-	-drive file=$@,format=qcow2,if=none,id=disk0,cache=none \
+    -netdev tap,id=hostnet0,vhost=on,script=./nat_up.py,downscript=./nat_down.py \
+    -device virtio-net-pci,netdev=hostnet0,id=net0,mac=52:54:00:1b:fa:b2 \
+	-drive file=$1,format=qcow2,if=none,id=disk0,cache=none \
 	-device virtio-blk,drive=disk0,bootindex=1 \
 	-device virtio-gpu-pci \
 	-vnc 0.0.0.0:0 \
+	-serial stdio \
 	-sandbox off \
 	-msg timestamp=on
